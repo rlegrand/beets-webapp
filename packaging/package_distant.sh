8ae77df3c7dff05005c831b9fc983316bd75ca1f
rlegrand <legrand.regis@gmail.com>
2019-05-29 23:03:05 +0200

    needed lines were removed, adding them back / build-v1

--- packaging/package_distant.sh
+++ packaging/package_distant.sh
@@ -4,7 +4,11 @@ set -x
 set -e
 
 IMAGE=$1
-[ -z "$IMAGE"] && ( echo "Image param missing" && exit -1 )
+[ -z "$IMAGE" ] && ( echo "Image param missing" && exit -1 )
+
+echo "Tag image"
+TAG=rlegrand/${IMAGE}:latest
+docker tag ${IMAGE} ${TAG}
 
 echo "Push image on dockerhub"
 
