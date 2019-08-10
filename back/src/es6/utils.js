9ceb598753786348a4430d9ffb1f895ce8c7e516
rlegrand <legrand.regis@gmail.com>
2019-08-10 16:22:49 +0200

    build-v1.0 /use my own noop operator

--- back/src/es6/utils.js
+++ back/src/es6/utils.js
@@ -2,6 +2,8 @@
 
 import winston from 'winston';
 
+import { tap } from 'rxjs/operators';
+
 class Utils{
     getDate = (dateStr) => {
         const regex = /(\d{4})-(\d{2})-(\d{2}) (\d{2}):(\d{2}):(\d{2})/;
